3e2b8321cf33943bb3aabab32582e1a73e163e1a
RRMaker <83099525+RRMaker@users.noreply.github.com>
2022-06-15 19:57:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+
+}
+function setup(){
+   canvas = createCanvas(400, 400);
+   canvas.center();
+   video = createCapture(VIDEO);
+   video.size(500, 500);
+}
+function draw(){
+    background("#6c6f8c");
+}
